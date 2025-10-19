7018ad6fa250a2e4123de70bf88855f25c083be2
ljurm <ljurmo@gmail.com>
2025-10-18 20:55:45 -0400

    settings screen buttons, needs ui repositioning still

--- BarPath/app/src/main/java/com/example/barpath/MainScreenViewModel.kt
+++ BarPath/app/src/main/java/com/example/barpath/MainScreenViewModel.kt
@@ -1,7 +1,11 @@
 package com.example.barpath
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
 
 class MainScreenViewModel: ViewModel() {
 
+
+    val selectedTheme = MutableList<Boolean?>(2) { false }
+    val _selectedTheme = MutableStateFlow(selectedTheme)
 }
--- BarPath/app/src/main/java/com/example/barpath/SettingsScreen.kt
+++ BarPath/app/src/main/java/com/example/barpath/SettingsScreen.kt
@@ -1,16 +1,86 @@
 package com.example.barpath
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+
 
 @Composable
-fun SettingsScreen(vm: MainScreenViewModel, onNavigateBack: () -> Unit ={}){
+fun SettingsScreen(vm: MainScreenViewModel, onNavigateBack: () -> Unit ={}) {
+    var _selectedTheme by remember { mutableStateOf(false) }
 
-    Button(
-        onClick = {onNavigateBack()}
-    ) {
-        Text("Back")
+    Column(){
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ){
+
+            Button(
+                onClick = { onNavigateBack() }
+            ) {
+                Text("Back")
+            }
+
+            Button(
+                onClick = { onNavigateBack() }
+            ) {
+                Text("Delete History")
+            }
+
+
+
+        Text(
+            modifier = Modifier
+                .padding(start = 80.dp, top = 40.dp),
+            text = "Select app theme:"
+        )
+
+        IconButton(
+            modifier = Modifier
+                .padding(top = 40.dp),
+            onClick = { _selectedTheme = !_selectedTheme }) {
+            Icon(Icons.Default.KeyboardArrowDown, contentDescription = "More options")
+        }
+            Box(
+                modifier = Modifier
+                    .padding(start = 70.dp, top = 70.dp)
+
+            ){
+        DropdownMenu(
+            expanded = _selectedTheme,
+            onDismissRequest = { _selectedTheme = false }
+        ) {
+            DropdownMenuItem(
+                text = { Text("Light Mode") },
+                onClick = {}
+            )
+            DropdownMenuItem(
+                text = { Text("Dark Mode") },
+                onClick = {}
+            )
+        }
+        }
     }
-
 }
+
+    }
