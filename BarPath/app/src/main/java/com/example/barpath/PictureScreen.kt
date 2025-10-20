26220a3c31751b30545b74c00dcb79816576f352
Nikolai Escondo <nescondo2@gmail.com>
2025-10-19 23:10:19 -0400

    added camera permissions on clicking 'Take Photo' button in Picture Screen

--- BarPath/app/src/main/java/com/example/barpath/PictureScreen.kt
+++ BarPath/app/src/main/java/com/example/barpath/PictureScreen.kt
@@ -1,5 +1,9 @@
 package com.example.barpath
 
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -11,24 +15,55 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
 
 @Composable
-fun PictureScreen (vm: MainScreenViewModel, onNavigateBack: () -> Unit ={}) {
+fun PictureScreen (vm: MainScreenViewModel, onNavigateBack: () -> Unit) {
+
+    val context = LocalContext.current
+    var hasCameraPermission by remember {
+        mutableStateOf(
+            ContextCompat.checkSelfPermission(
+                context,
+                Manifest.permission.CAMERA
+            ) == PackageManager.PERMISSION_GRANTED
+        )
+    }
+
+    val permissionLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        hasCameraPermission = isGranted
+        if (isGranted) {
+            // Permission was granted. You can now launch the camera.
+        }
+        else {
+            // Permission was denied. You can show a message to the user.
+        }
+    }
 
     Column(
 
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.spacedBy(12.dp),
-        modifier = Modifier.fillMaxWidth()
+        modifier = Modifier
+            .fillMaxWidth()
             .background(vm._color1.value)
             .fillMaxHeight(),
     ) {
         Spacer(Modifier.height(100.dp))
         Button(
-            onClick = {},
+            onClick = {
+                permissionLauncher.launch(Manifest.permission.CAMERA)
+            },
             modifier = Modifier.fillMaxWidth()
         ) {
             Text("Take Photo")
